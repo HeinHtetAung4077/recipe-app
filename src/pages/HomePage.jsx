bae825c6b53296c6c4a1e2d200cdc8213d014d45
Hein Htet Aung <heinhtetaung405@gmail.coim>
2024-07-02 15:14:24 +0530

    2- App completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,46 @@
 import { Search } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RecipeCard from "../components/RecipeCard";
+import { getRandomColor } from "../lib/utils";
+
+const APP_ID = import.meta.env.VITE_APP_ID;
+const APP_KEY = import.meta.env.VITE_APP_KEY;
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRecipe = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+    try {
+      const res = await fetch(
+        `https://api.edamam.com/api/recipes/v2/?app_id=${APP_ID}&app_key=${APP_KEY}&q=${searchQuery}&type=public`
+      );
+      const data = await res.json();
+      // console.log(data);
+      setRecipes(data.hits);
+      console.log(recipes);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipe("chicken");
+  }, []);
+
+  const handleSearchRecipe = (e) => {
+    e.preventDefault();
+    fetchRecipe(e.target[0].value);
+  }
+
   return (
     <div className="bg-[#faf9fb] flex-1 p-10">
       <div className="max-w-screen-lg mx-auto">
-        <form>
+        <form onSubmit={handleSearchRecipe}>
           <label className="input shadow-md flex items-center gap-2">
             <Search size={"24"} />
             <input
@@ -24,11 +58,23 @@ const HomePage = () => {
         </p>
 
         <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3">
-          {/* 1st Recipe */}
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
+          
+        {!loading &&
+						recipes.map(({ recipe }, index) => (
+							<RecipeCard key={index} recipe={recipe} {...getRandomColor()} />
+						))}
+
+          {loading &&
+						[...Array(9)].map((_, index) => (
+							<div key={index} className='flex flex-col gap-4 w-full'>
+								<div className='skeleton h-32 w-full'></div>
+								<div className='flex justify-between'>
+									<div className='skeleton h-4 w-28'></div>
+									<div className='skeleton h-4 w-24'></div>
+								</div>
+								<div className='skeleton h-4 w-1/2'></div>
+							</div>
+						))}
         </div>
       </div>
     </div>
